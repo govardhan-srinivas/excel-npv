b43dea421a187061cf88184518e78224e45da266
Govardhan S <govardhan.s92@gmail.com>
2017-12-01 08:47:06 +0530

    small fixes

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-/*
+	/*
 * Author: Govardhan
 * Title: excel-npv
 * Description: Calculate NPV as per excel function
 * Date: 11-10-2017
 */
-var Finance = function() {};
+(function(){
+	let Finance = function() {};
 
-Finance.NPV = function(rate)  {
-    let npv = 0;
-    rate /=100;
-    for (var i = 1; i < arguments.length; i++) {
-        npv += arguments[i] / Math.pow(1 + rate, i);
-    }
-    return Math.round(npv * 100) / 100;
-}
-module.exports = Finance;
+	Finance.NPV = function()  {
+	    let npv = 0;
+	    discountRate = arguments[0]/100; // Converting discount rate into percent
+	    for (let i = 1; i < arguments.length; i++) {
+	        npv += arguments[i] / Math.pow(1 + discountRate, i);
+	    }
+	    return Math.round(npv * 100) / 100;
+	}
+
+	module.exports = Finance;
+})();
